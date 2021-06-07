54439a7b33bc03c7301351e6898d257e8a6d055c
mohparker <80479690+mohparker@users.noreply.github.com>
2021-06-07 16:42:38 +0200

    code evidence 219154554

---
+++ code.js
@@ -0,0 +1,20 @@
+var feild = document.querySelector('textarea');
+
+var backUp = feild.getAttribute('placeholder');
+
+var btn = document.querySelector('.btn');
+
+var clear = document.getElementById('clear')
+
+feild.onfocus = function () {
+    this.setAttribute('placeholder', '');
+    this.style.borderColor = 'Blue';
+    btn.style.display = 'block'
+}
+
+
+
+clear.onclick = function () {
+    btn.style.display = 'none';
+    feild.value = '';
+}
