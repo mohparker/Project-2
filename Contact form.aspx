94403c6a333ce721a84363777a2ef4864b22fec4
DieuciND <80687555+DieuciND@users.noreply.github.com>
2021-08-28 17:00:39 +0200

    Contact form by 220169136

---
+++ Contact form.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <form id="form" runat="server">
+        <div>
+            <div class="conatct-form">
+                <div class="input">
+                    <h1>Contact Us</h1>
+
+                    <h3>
+                        <asp:Label ID="confirm" runat="server" Text=""></asp:Label>
+                   </h3>
+                      
+
+
+                    <p>Name*</p>
+
+                    <asp:TextBox ID="name" runat="server" placeholder="Enter name" Required></asp:TextBox>
+
+
+
+                    <p>Email*</p>
+                    <asp:TextBox ID="email" runat="server" placeholder="Enter  email" Required></asp:TextBox>
+
+
+
+                    <p>Phone (optional)</p>
+                    <asp:TextBox ID="phone" runat="server" placeholder="Enter  phone" ></asp:TextBox>
+
+
+
+                     <p>Subject*</p>
+                    <asp:TextBox ID="subject" runat="server" placeholder="Enter  your subject" Required></asp:TextBox>
+
+
+
+
+                     <p>Message*</p>
+                    <asp:TextBox ID="message" runat="server" placeholder="type  your message..." Required TextMode="MultiLine" Rows="4"></asp:TextBox>
+
+                   
+
+
+                    <asp:Button ID="Submit" runat="server" Text="submit" OnClick="btn_Submit_Click" />
+                
+
+                </div>
+            </div>
+          
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Diagnostics; // This class allows me to debug my code.
+
+
+//Dieuci djate Nsibu 220169136
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_Submit_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+            var from = "forexnewshouse@gmail.com"; // here i enter the sender email 
+            var to = "forexnewshouse@gmail.com";
+            const string password = "Forex2021";
+           
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential(from, password); // here i enter the actual password of the sender email.
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+
+
+
+            //Setting From , 
+            mail.From = new MailAddress(from, "forexnewshouse");
+            mail.To.Add(new MailAddress(to));
+
+
+            //here i set message 
+            mail.Subject = subject.Text.ToString();
+            mail.Body = ("Name :  "+name.Text+ "\n" + "Email :  "+email.Text+ "\n"+ "Phone :  "+phone.Text+ "\n" + "\n" + message.Text);
+
+            smtpClient.Send(mail);
+            confirm.Text = "Your message has been sent, Thank you for contacting us";
+            name.Text = "";
+            email.Text = "";
+            phone.Text = "";
+            message.Text = "";
+        }
+        catch (Exception)
+        {
+            confirm.Text = "Sorry Something went wrong!!";
+        }
+    }
+}
